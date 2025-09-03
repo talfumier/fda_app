2ac486524ad009dd90ed6056fc2c1fe8b29c0f5c
talfumier <henri.talfumier@gmail.com>
2025-09-03 19:06:23 +0200

    Reworked api url in various environments.

--- src/services/httpEntities.js
+++ src/services/httpEntities.js
@@ -1,37 +1,37 @@
 import http from './httpService.js'
+import { environment } from '@/environment/environment.js'
 
-const api = 'http://localhost:3000/api'
-
+const api = environment.api_url
 export function getEntities(entity, signal) {
-  return http.get(`${api}/entities/${entity}`, {
+  return http.get(`${api()}/entities/${entity}`, {
     signal,
   })
 }
 export function getEntitiesBySql(stored_proc, params, paramsValues, token, signal) {
-  return http.get(`${api}/sql-entities/${stored_proc}/${params}/${paramsValues}`, {
+  return http.get(`${api()}/sql-entities/${stored_proc}/${params}/${paramsValues}`, {
     headers: { 'x-auth-token': token },
     signal,
   })
 }
 export function getEntity(entity, id, signal) {
-  return http.get(`${api}/entities/${entity}/${id}`, {
+  return http.get(`${api()}/entities/${entity}/${id}`, {
     signal,
   })
 }
 export function patchEntity(entity, id, data, token, signal) {
-  return http.patch(`${api}/entities/${entity}/${id}`, data, {
+  return http.patch(`${api()}/entities/${entity}/${id}`, data, {
     headers: { 'x-auth-token': token },
     signal,
   })
 }
 export function postEntity(entity, data, token, signal) {
-  return http.post(`${api}/entities/${entity}`, data, {
+  return http.post(`${api()}/entities/${entity}`, data, {
     headers: { 'x-auth-token': token },
     signal,
   })
 }
 export function deleteEntity(entity, id, token, signal) {
-  return http.delete(`${api}/entities/${entity}/${id}`, {
+  return http.delete(`${api()}/entities/${entity}/${id}`, {
     headers: { 'x-auth-token': token },
     signal,
   })
