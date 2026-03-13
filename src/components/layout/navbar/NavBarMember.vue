d58f18df405bda24a732955cd603075dab3ed0d6
talfumier <henri.talfumier@gmail.com>
2026-03-13 17:32:52 +0100

    Member space responsiveness.

--- src/components/layout/navbar/NavBarMember.vue
+++ src/components/layout/navbar/NavBarMember.vue
@@ -1,25 +1,16 @@
 <script setup>
-  import { ref,watch,inject,computed, onMounted, onUnmounted } from 'vue'  
+  import { ref,inject,computed, onMounted, onUnmounted } from 'vue'  
   import _ from 'lodash'
   import { environment } from '@/config/environment.js'
   import Tooltip from '../../common/Tooltip.vue'
-  import public_items from './nav-items-public.json'
   import member_items from "./nav-items-member.json"
   import NavBarItem from './NavBarItem.vue'
   import { getEntitiesBySql } from '@/services/httpEntities.js'
   
   const props = defineProps({
-    type:{type:String},
     wrap:{type:Boolean,default:false}
-  })
+  })  
   
-  const ctrl=new AbortController()  
-  watch(
-    () => props.type, //getter function
-    async() => {  
-      if(props.type==='member') await fetch(ctrl.signal)
-    }
-  )
   const url=ref(null)
   const isRotated = ref(false)
   function rotateIcon() {
@@ -27,7 +18,6 @@
   }
   const {token,decoded} = inject('userCookie')
   const roleFilteredItems=computed(() => {
-    if(props.type==='public') return public_items
     return _.filter(member_items,(item) => {
       if(decoded.value.idRole===7 && item['dev-test']) return !environment.production
       return decoded.value.idRole?item.roles.includes(decoded.value.idRole):false
@@ -46,8 +36,8 @@
       url.value=res.data[0][0]?.url
     }
   }
+  const ctrl=new AbortController()  
   onMounted(async() => {
-    if(props.type==='public') return
     await fetch(ctrl.signal)
   })
   onUnmounted(() => { // clean-up code after component has unmounted
@@ -56,31 +46,30 @@
 </script>
 
 <template>
-    <nav :class="[isRotated || $q.screen.width<850?'folded':'',type]">
+    <nav :class="[isRotated || $q.screen.width<=850?'folded':'']">
       <ul>
         <q-icon v-if="$q.screen.width>850"
           class="btn-fold" 
           name="keyboard_double_arrow_left" 
           size="md"
           @click="rotateIcon">
-        </q-icon>        
-        <hr v-if="type==='public'" ></hr>
-        <li>
-          <RouterLink v-if="type==='member'" to="/member/user" tabindex="-1">
-            <img v-if="url" class="avatar" :src="url" alt='avatar'>
-            <div v-if="!url" class="avatar">
-              <q-icon  class='avatar' name="account_circle" ></q-icon>
+        </q-icon>  
+        <li class="avatar">
+          <RouterLink to="/member/user" tabindex="-1">
+            <div class="avatar">
+              <img v-if="url" class="avatar" :src="url" alt='avatar'>
+              <q-icon  v-else class='avatar' name="account_circle" ></q-icon>
             </div>
             <Tooltip :tt_text="$t('comps.header.settings-ico.tip')"></Tooltip>
           </RouterLink>
+        <hr class="avatar"></hr>
         </li>
-        <hr v-if="type==='member'" ></hr>
         <li v-for="(item, idx) in roleFilteredItems" :key="idx">
           <NavBarItem 
             :key="roleFilteredItems"
             source='member'
             :item="item" 
-            :isRotated="isRotated || $q.screen.width<850" 
+            :isRotated="isRotated || $q.screen.width<=850" 
           />
         </li>
 
@@ -96,15 +85,9 @@
     border: 1px solid #ddd;
     transition: width 0.6s ease;
   }
-  nav.public hr {
-    border-color: transparent;
-    border-width: 5px;
-  }
   nav.folded {
-    width:95px;
-  }
-  nav.public.folded {
-    width: 60px;
+    padding-top: 50px;
+    width:50px;
   }
   .btn-fold {
     position:absolute;
@@ -127,17 +110,11 @@
   nav.folded .q-icon.btn-fold {
     transform: rotate(-180deg);
   }
-  a.router-link-active img.avatar {
+  a.router-link-active div.avatar {
     border:solid 3px var(--orange);
-  }
+  } 
   div.avatar {    
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    width:80px;
-    height:80px;
-    margin:0 auto;
-    overflow: hidden;
+    display:none;
   }
   .q-icon.avatar {
     color:var(--orange);
@@ -149,6 +126,7 @@
     border-radius: 50%;    
   }
   hr {
+    display:none;
     width:100%;
   }
   ul { 
@@ -168,8 +146,6 @@
     width:100%;
   }
   li:has(img,.q-icon.avatar) {
-    margin: 1rem auto;
-    margin-bottom:0;
     text-align: center;
     padding:0;
   }
@@ -180,4 +156,24 @@
     object-fit: cover;
     cursor: pointer;
   }
+  @media screen and (min-width: 500px) {  
+    nav.folded {
+      padding-top: 0;
+      width:95px;
+    }     
+    div.avatar {
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      width:80px;
+      height:80px;
+      overflow: hidden;
+      border:none;
+      margin:10px auto 0;
+    }
+    hr {
+      display:block;
+    }
+    
+  } 
 </style>
